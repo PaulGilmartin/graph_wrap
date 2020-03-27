aea531929f85ed77f8b43dd2964c7ebf7dda1588
Paul Gilmartin <paulgilmartin@Pauls-MacBook-Pro.local>
2020-03-27 13:14:24 +0100

    Add setup and license

---
+++ setup.py
@@ -0,0 +1,66 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+Modified by Madoshakalaka@Github (dependency links added)
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="graph_wrap",  # Required
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version="0.0.1",
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description="Transform a tastypie REST-based API into a fully compliant GraphQL API.",
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",
+    url="https://github.com/PaulGilmartin/graph_wrap",
+    author="Paul Gilmartin",
+    author_email="paul.gilmartin89@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="tastypie graphene django graphql rest api",
+    packages=find_packages(),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        "django-tastypie==0.14.3",
+        "graphene-django==2.9.0",
+    ],
+    project_urls={
+        "Source": "https://github.com/PaulGilmartin/graph_wrap",
+    },
+)
