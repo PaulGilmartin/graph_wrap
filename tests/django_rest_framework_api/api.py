23d51a6b945c2c7bb9142f6d02f6105f15d588c1
Paul Gilmartin <paulgilmartin@Pauls-MacBook-Pro.local>
2021-03-09 20:26:43 +0100

    Revert changes to test api

--- tests/django_rest_framework_api/api.py
+++ tests/django_rest_framework_api/api.py
@@ -10,27 +10,59 @@ from tests.models import Author, Post
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'is_staff']
+        exclude = ('password',)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     entries = serializers.PrimaryKeyRelatedField(
         many=True, queryset=Post.objects.all())
+    amount_of_entries = serializers.SerializerMethodField()
+    name = serializers.CharField(source='get_name')
 
     class Meta:
         model = Author
-        fields = ['name', 'age', 'active', 'profile_picture', 'user', 'entries']
+        fields = [
+            'name',
+            'age',
+            'active',
+            'profile_picture',
+            'user',
+            'entries',
+            'amount_of_entries',
+        ]
+
+    def get_amount_of_entries(self, obj):
+        return obj.entries.count()
+
+
+class WrittenBySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='get_name')
+
+    class Meta:
+        model = Author
+        fields = ['name']
 
 
 class PostSerializer(serializers.ModelSerializer):
-    written_by = serializers.HyperlinkedRelatedField(
-        view_name='author-detail', read_only=True)
+    written_by = WrittenBySerializer(source='author')
+    author = serializers.HyperlinkedRelatedField(
+        view_name='author-detail',
+        read_only=True
+    )
 
     class Meta:
         model = Post
         depth = 3
-        fields = ['written_by', 'content', 'date', 'files', 'rating']
+        fields = [
+            'written_by',
+            'author',
+            'content',
+            'author',
+            'date',
+            'rating',
+            'files',
+        ]
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
